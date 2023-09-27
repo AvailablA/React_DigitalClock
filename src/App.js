fc0dfd7c6b858b8cf17752deb0593cad110d42b3
AvailablA <manohar.shewale77@gmail.com>
2023-09-27 10:58:10 +0530

    The digital clock demo16 add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = ()=>
+{
+
+
+  let time = new Date().toLocaleTimeString();
+  
+  const[curTime,setTime] = useState(time);
+
+  const updateTime = ()=>
+  {
+    time = new Date().toLocaleTimeString();
+
+    setTime(time);
+  };
+
+  setInterval(updateTime,1000)
+  
+  
+  return(
+    <>
+        <h1>{time}</h1>
+    </>
   );
 }
 
